bf71553522cb9af24efc3f5cac3226b59eebd6bb
thinhphungdev <phungthinh0946519356@gmail.com>
2024-05-22 12:02:10 +0700

    feat: init project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import GlobalStyle from './styles/GlobalStyles';
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <div>
+        <h1>Hehe</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
