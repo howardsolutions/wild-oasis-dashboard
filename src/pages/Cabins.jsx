8e14ac9a529628d6bbd4c050b73b0652d6a9434d
thinhphungdev <phungthinh0946519356@gmail.com>
2024-05-22 15:24:04 +0700

    setup relational db and integrate into react

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,10 +1,16 @@
-import Heading from "../ui/Heading";
-import Row from "../ui/Row";
+import { useEffect } from 'react';
+import Heading from '../ui/Heading';
+import Row from '../ui/Row';
+import { getCabins } from '../services/apiCabins';
 
 function Cabins() {
+  useEffect(() => {
+    getCabins().then((data) => console.log(data));
+  }, []);
+
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
+    <Row type='horizontal'>
+      <Heading as='h1'>All cabins</Heading>
       <p>TEST</p>
     </Row>
   );
---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+const supabaseUrl = 'https://tysgujeglozoasmyjsbj.supabase.co';
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR5c2d1amVnbG96b2FzbXlqc2JqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYzNjM1NzksImV4cCI6MjAzMTkzOTU3OX0.fEuzmqewq8eZzkbUtVKQW1_z4hsf0GYoYL_y2TZnSgs';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
