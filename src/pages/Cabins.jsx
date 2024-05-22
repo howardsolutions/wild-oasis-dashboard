ecfff7da31e8a43620d29e43b1b38fdb9f8dcf83
thinhphungdev <phungthinh0946519356@gmail.com>
2024-05-22 17:05:13 +0700

    feat: fetch cabins data

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import Heading from '../ui/Heading';
 import Row from '../ui/Row';
 import { getCabins } from '../services/apiCabins';
+import CabinTable from '../features/cabins/CabinTable';
 
 function Cabins() {
   useEffect(() => {
@@ -9,9 +10,16 @@ function Cabins() {
   }, []);
 
   return (
-    <Row type='horizontal'>
-      <Heading as='h1'>All cabins</Heading>
-    </Row>
+    <>
+      <Row type='horizontal'>
+        <Heading as='h1'>All cabins</Heading>
+        <p>Filter / Sort</p>
+      </Row>
+
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
