c453e3a1dd5b885bd2daf9f540b2ec6b5953a93c
thinhphungdev <phungthinh0946519356@gmail.com>
2024-05-26 17:18:06 +0700

    feat UI: Header

--- src/features/authentication/queries/useSignUp.js
+++ src/features/authentication/queries/useSignUp.js
@@ -7,7 +7,7 @@ export function useSignup() {
     mutationFn: signupApi,
     onSuccess: () => {
       toast.success(
-        "Account successfully created! Please verufy the new account from the user's email address."
+        "Account successfully created! Please verify the new account from the user's email address."
       );
     },
   });
