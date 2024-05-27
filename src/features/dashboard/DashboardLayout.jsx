c671b51b3efac389829ef06a9113f177749dd8e0
thinhphungdev <phungthinh0946519356@gmail.com>
2024-05-27 16:17:16 +0700

    feat: calculate recent bookings and recent stays

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,13 @@
-import styled from "styled-components";
+/* eslint-disable no-unused-vars */
+import styled from 'styled-components';
+// import { useRecentStays } from './useRecentStays';
+// import { useRecentBookings } from './useRecentBookings';
+// import Spinner from '../../ui/Spinner';
+// import Stats from './Stats';
+// import { useCabins } from '../cabins/useCabins';
+// import SalesChart from './SalesChart';
+// import DurationChart from './DurationChart';
+// import TodayActivity from '../check-in-out/TodayActivity';
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +15,21 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  // const { bookings, isLoading: isLoading1 } = useRecentBookings();
+  // const { confirmedStays, isLoading: isLoading2, numDays } = useRecentStays();
+  // const { cabins, isLoading: isLoading3 } = useCabins();
+
+  // if (isLoading1 || isLoading2 || isLoading3) return <Spinner />;
+
+  return (
+    <StyledDashboardLayout>
+      <div>Stats</div>
+      <div>Today activity</div>
+      <div>Hihi</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
